9f6667cfcca862a71282061b74eb01bc667a80ed
Pau March <pmarch@minsait.com>
2022-11-15 09:13:07 +0100

    initial commit

---
+++ api.js
@@ -0,0 +1,6 @@
+// Pedir datos de una api y pintar una lista.
+// Mapear los datos de la api a un objeto que necesites.
+// No usar estilos, solo lógica.
+// Al darle click a un elemento abrir modal o detalle que pinte la información completa.
+// Para importar ficheros utilizar Import y Export.
+// Extra: Si sobra tiempo, añadir paginación al listado.
